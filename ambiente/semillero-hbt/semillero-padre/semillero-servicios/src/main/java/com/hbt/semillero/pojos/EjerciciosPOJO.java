dca3b9e33ee1882cc5df61e7f8030330301a2d25
GaboH97 <gabriel970826@gmail.com>
2019-11-13 19:18:29 -0500

    Ejercicio 3

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/pojos/EjerciciosPOJO.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/pojos/EjerciciosPOJO.java
@@ -4,18 +4,36 @@
 package com.hbt.semillero.pojos;
 
 /**
- * <b>Descripción:<b> Clase que determina
- * <b>Caso de Uso:<b> 
+ * <b>Descripción:<b> Clase que determina <b>Caso de Uso:<b>
+ * 
  * @author Gabriel Huertas
- * @version 
+ * @version
  */
-
 
 public class EjerciciosPOJO {
 
-	
 	public boolean ejercicio1(String identifier) {
 		return Character.isJavaIdentifierStart(identifier.charAt(0))
-				&& identifier.substring(1,identifier.length()-1).chars().allMatch(Character::isJavaIdentifierPart);
+				&& identifier.substring(1, identifier.length() - 1).chars().allMatch(Character::isJavaIdentifierPart);
+	}
+	
+	/**
+	 * 
+	 * Metodo encargado de mirar si un número es primo
+	 * <b>Caso de Uso</b>
+	 * @author Gabriel Huertas 
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public boolean isprime(int number) {
+		boolean isPrime = true;
+		for (int i = 2; i <= number / 2; i++) {
+			if (number % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		return isPrime;
 	}
 }
--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjerciciosTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjerciciosTest.java
@@ -6,26 +6,46 @@ package com.hbt.semillero.servicios;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.hbt.semillero.pojos.EjerciciosPOJO;
+
 /**
- * <b>Descripción:<b> Clase que determina
- * <b>Caso de Uso:<b> 
+ * <b>Descripción:<b> Clase que determina <b>Caso de Uso:<b>
+ * 
  * @author Gabriel Huertas
- * @version 
+ * @version
  */
 public class EjerciciosTest {
 
 	private static String brand;
 	private static boolean empty;
-	
+
 	@Test
 	public void ejercicio1() {
 		String caso1 = "A$B";
 	}
-	
+
 	@Test
 	public static void ejercicio2() {
-		System.out.println("Empty="+empty);
-		System.out.println("Brand"+brand);
-		
+		System.out.println("Empty=" + empty);
+		System.out.println("Brand" + brand);
 	}
+
+	@Test
+	public void ejercicio3() {
+		EjerciciosPOJO ejerciciosPOJO = new EjerciciosPOJO();
+		
+		//Es primo
+		int number = 5;
+		Assert.assertEquals(ejerciciosPOJO.isprime(number), true);
+		
+		//No es primo
+		number = 222;
+		Assert.assertEquals(ejerciciosPOJO.isprime(number), false);
+		
+		//No es primo
+		number = 0;
+		Assert.assertEquals(ejerciciosPOJO.isprime(number), false);
+
+	}
+
 }
